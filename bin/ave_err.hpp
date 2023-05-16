50f748ae71b26a7dd2bceea524b20093670c3864
Matteo Di Carlo <matteo.dicarlo93@gmail.com>
2023-05-16 23:15:36 +0100

    clean up: removing comments

--- bin/ave_err.hpp
+++ bin/ave_err.hpp
@@ -4,67 +4,6 @@
 #include <tuple>
 #include "aliases.hpp"
 
-//template <class T>
-//void compute_ave( T &ave, T &sqr_ave, vector<T> vec)
-//{
-//    for(int i=0; i<vec.size(); i++)
-//        compute_ave_internal();
-//}
-//
-//
-//void get_sizes(double vec, vector<size_t> &sizes);
-//
-//template <class T>
-//void get_sizes(valarray<T> vec, vector<size_t> &sizes)
-//{
-//    sizes.push_back(vec.size());
-//    cout<<vec.size()<<endl;
-//
-//    get_sizes(vec[0],sizes);
-//}
-//
-//template <class T>
-//void get_sizes(vector<T> vec, vector<size_t> &sizes)
-//{
-//    sizes.push_back(vec.size());
-//    cout<<vec.size()<<endl;
-//
-//    get_sizes(vec[0],sizes);
-//}
-//
-//template <class T, int pos_jack>
-//tuple<T,T> ave_err(vector<T> vec)
-//{
-//    for(int i=0; i<vec.size(); i++)
-//        ave_err<pos_jack-1>(vec[i]);
-//}
-//
-//template <class T>
-//tuple<T,T> ave_err<T,0>(vector<T> vec)
-//{
-//    vector<int> sizes, new_sizes;
-//
-//    get_sizes(vec,sizes);
-//
-//    for(int i=0;i<sizes.size();i++)
-//        if(i!=col)
-//            new_sizes.push_back(sizes[i]);
-//
-//    T ave, sqr_ave, err;
-//
-//    allocate_vec(ave,new_sizes);
-//    allocate_vec(sqr_ave,new_sizes);
-//    allocate_vec(err,new_sizes);
-//}
-//
-//template <class T>///////////////
-//tuple<T,T> ave_err(valarray<T> vec, int col)
-//{
-//    vector<int> sizes;
-//
-//    get_sizes(vec,sizes);
-//}
-
 // average sigma
 tuple<vvvvd_t,vvvvd_t> ave_err(vector<vvvvd_t> sigma);
 
