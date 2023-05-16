2bd9d2f3f31c0ef1ca1417b876cbc0e461bb7366
Matteo Di Carlo <matteo.dicarlo93@gmail.com>
2023-05-17 00:06:40 +0100

    clean up: removing unused namespace

--- bin/Zq.hpp
+++ bin/Zq.hpp
@@ -2,15 +2,4 @@
 #define ZQ_HPP
 
 
-#include "global.hpp"
-
-namespace zq
-{
-    enum ins{LO,EM};
-    int nins;
-//    if(clover_analysis)
-//    {nins=1;}
-//    else
-//    {nins=2;}
-}
 #endif
